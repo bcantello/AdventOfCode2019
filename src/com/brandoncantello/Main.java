672cb97bd0c8339292f2073fa9891136685987ee
Brandon <bcantello@gmail.com>
2019-12-24 13:47:11 -0800

    Initial Commit with solution

---
+++ src/com/brandoncantello/Main.java
@@ -0,0 +1,15 @@
+package com.brandoncantello;
+
+import com.brandoncantello.problem1.FuelCalculator;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        /**
+         * Run Solution to Problem 1:
+         */
+        FuelCalculator fuelCalculator = new FuelCalculator();
+        System.out.println("Solution 1: " + fuelCalculator.calculator());
+    }
+}
